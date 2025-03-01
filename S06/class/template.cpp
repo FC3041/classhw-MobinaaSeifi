b2b04dfc86abde09227e135f664b3c74a8d21ce1
MobinaaSeifi <mobinaaseifi@gmail.com>
2025-02-28 22:48:27 -0800

    add

---
+++ S06/class/template.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+template <typename T> 
+
+class MyList
+{
+    public:
+        int m_size;
+        int m_capacity;
+       T* m_pNums;
+
+        MyList():m_size(0),m_capacity(0),m_pNums(nullptr){};
+
+        MyList(int size, int capacity , const T* nums)
+        :m_size(size), m_capacity(capacity)
+        {
+            m_pNums = (T*)malloc(sizeof(int)*capacity);
+            for (int i=0; i<m_size;i++)
+            {
+                m_pNums[i] = nums[i];
+            }
+        }
+
+        void print_list()
+        {
+            cout << "size:" << m_size << " capacity:" << m_capacity << endl;
+            for(int i=0;i<m_size;i++)
+            {
+                cout << m_pNums[i] << "-";
+            }
+            cout<< endl;
+
+        }
+
+        int len(){return m_size;}
+
+        void append(int value)
+        {
+            if (m_size == m_capacity)
+            {
+                if (m_size ==0){resize(1);}
+                else
+                {
+                    resize(m_capacity*2);
+                }
+            }
+            m_pNums[m_size] = value;
+            m_size ++;
+        }
+
+        void insert(int index, int value)
+        {
+            if (m_size==m_capacity)
+            {
+                if (m_size==0){resize(1);}
+                else{resize(m_capacity*2);}
+            }
+            for(int i=m_size; i>=index ;i--)
+            {
+                m_pNums[i] = m_pNums[i-1]; 
+            }
+            m_pNums[index] = value;
+            m_size++;
+        }
+
+        void clear()
+        {
+            m_size = 0;
+            resize(1);
+        }
+
+        void erase(int index)
+        {
+            for(int i=index;i<m_size-1;i++)
+            {
+                m_pNums[i] = m_pNums[i+1];
+            }
+
+            m_size --;
+        }
+        void reverse()
+        {
+            int i;
+            for(i=0; i<m_size/2; i++)
+            {
+                m_pNums[i] == m_pNums[m_size-i];
+            }
+        }
+
+
+        //----------------//reverse//-----------------//
+
+    private:
+        void resize(int newsize)
+        {
+            int* newNums = (int*)malloc(sizeof(int)*(newsize));
+            for(int i=0; i<m_size;i++)
+            {
+                newNums[i] = m_pNums[i];
+            }
+            free(m_pNums);
+            m_pNums = newNums;
+            m_capacity = newsize;
+
+        }
+
+};
+
+int main()
+{
+    MyList l1;
+
+    int nums[5] = {1,2,3,4,5};
+    MyList l2(5, 10, nums);
+
+    l2.print_list();
+    l2.append(19);
+
+    l2.print_list();
+
+    l2.insert(1, 55);
+    l2.print_list();
+
+    l2.erase(1);
+    l2.print_list();
+
+    int nums2[6]= {6,5,4,3,2,1};
+    MyList l3(6,10,nums);
+
+    l3.reverse();
+    l3.print_list();
+
+    return 0;
+}
