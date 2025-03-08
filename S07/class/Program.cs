f386919d6ddf9a9d79f66a30bc513c6ace08556c
MobinaaSeifi <mobinaaseifi@gmail.com>
2025-03-08 14:28:03 -0800

    add

---
+++ S07/class/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace class;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=0; i<1000000;i++)
+        {
+            int nums = new int [1000*1000];
+            Thread.Sleep(1);
+        }
+
+        Console.WriteLine("Hello, World!");
+    }
+}
