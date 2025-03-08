f758c456355203a829761faa99e788d961efb624
MobinaaSeifi <mobinaaseifi@gmail.com>
2025-03-07 23:31:32 -0800

    add

---
+++ S08/class/s08con.test/Test1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace s08con.test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+
+    // public void TestMethod2()
+    // {
+    //     int result = Program.add(3,5);
+    //     Assert.AreEqual(result,2);
+    // }
+}
---
+++ S08/class/s8con/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace s8con;
+
+public class Program
+{
+    public static int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    static void reverse(string s, out string srev)
+    {
+        srev = "";
+        foreach(char c in s)
+        {
+            srev = c+ srev;
+        }
+    }
+
+    // static void reverse_sentence(string s, out string senrev)
+    // {
+
+    // }
+    public static void preper_user(string s, out string sout)
+    {
+        sout = "user: " + s;
+    }
+    static void Main(string[] args)
+    {
+        string name = "ali";
+        out string srev ;
+        reverse(name, out srev);
+        System.Console.WriteLine(srev);    
+    }
+    static void Main2(string[] args)
+    {
+        int sum = 0;
+        while(true)
+        {
+            System.Console.WriteLine("Nums?");
+            string s = Console.ReadLine();
+            if(string.IsNullOrEmpty(s))
+                break;
+
+            sum += int.Parse(s);
+            // Console.WriteLine("Hello, Wo rld!");
+            int n;
+            if(! int.TryParse(s, out n))
+            {
+                System.Console.WriteLine( "wrong, try again");
+                continue;
+            }
+
+        }
+    }
+}
