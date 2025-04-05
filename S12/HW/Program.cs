0a7c22c318ba28fa3545928c06c4fff944d9f6c4
MobinaaSeifi <mobinaaseifi@gmail.com>
2025-04-05 11:38:21 -0700

    interface

---
+++ S12/HW/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace S12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICar[] machines = new ICar[]
+        {
+            new Cars(2,4,0),
+            new Cars(4,3,0)
+        };
+ 
+        
+        for(int i=0; i<machines.Length; i++)
+        {
+            System.Console.WriteLine(machines[i].v());
+            System.Console.WriteLine(machines[i].x());
+        }
+        
+    }
+}
